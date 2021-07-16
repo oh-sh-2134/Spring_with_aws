89c0bcde332dcfe2283342545ed74c6b4b2737ef
sh-oh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2021-07-16 23:07:19 +0900

    Application

---
+++ src/main/java/com/sh-oh-2134/Git/web/HelloController.java
@@ -0,0 +1,14 @@
+package web;
+
+import net.bytebuddy.implementation.bytecode.assign.TypeCasting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // 컨트롤러를 JSON 반환하는 컨트롤러로 만들어줌
+public class HelloController {
+
+    @GetMapping("/hello") // HTTP Method인 Get의 요청을 받을 수 있는 API를 만들어 줌
+    public String hello(){
+        return "hello";
+    }
+}
---
+++ src/test/java/com/sh-oh-2134/Git/HelloControllerTest.java
@@ -0,0 +1,7 @@
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.*;
+
+@ExtendWith()
+@WebM
+public class HelloControllerTest {
+}
